0b2d1f7daf320646c25448e1c35d8b94ceb0207d
Lorenzo Taglio <l.taglio@studenti.uniba.it>
2025-05-12 22:50:55 +0200

    Change socket conn

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://online-timer.onrender.com/:3000", {
   transports: ["websocket"],
 });
 

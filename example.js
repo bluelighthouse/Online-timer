d355f0d55ce5278e614e1412ee7b8059181363fc
Lorenzo Taglio <l.taglio@studenti.uniba.it>
2025-05-13 10:15:06 +0200

    change port

--- example.js
+++ example.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
 });
 
 // Avvio del server
-const PORT = 3000;
+const PORT = 5500;
 app.listen(PORT, () => {
   console.log(`Server in ascolto su http://localhost:${PORT}`);
 });
